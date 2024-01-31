ea4817c843c37f2a20c2d8a170318291c42dac00
ManuOmulo <omuloemmanuel@gmail.com>
2024-01-31 12:59:06 +0300

    Ammended the getProperties Interface method

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,19 @@ interface Mappable {
   Geometry geometryType();
   String marker();
   static void printProperties(Mappable item) {
-    System.out.printf("""
-        %s {"type": "%s", "label": "%s", "marker": "%s"}
+    if (item instanceof Building building) {
+      System.out.printf("""
+        %s {"type": "%s", "label": "%s", "marker": "%s", "name": "%s", "usage": "%s"}
         """,
-        Mappable.JSON_PROPERTY, item.geometryType(), item.label(), item.marker()
-    );
+          Mappable.JSON_PROPERTY, building.geometryType(), building.label(), building.marker(), building.getName(), building.getUsage()
+      );
+    } else if (item instanceof UtilityLine line) {
+      System.out.printf("""
+        %s {"type": "%s", "label": "%s", "marker": "%s", "name": "%s", "utility": "%s"}
+        """,
+          Mappable.JSON_PROPERTY, line.geometryType(), line.label(), line.marker(), line.getName(), line.getUtility()
+      );
+    }
   }
 
   default void toJson() {
